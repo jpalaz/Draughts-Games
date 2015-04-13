5604663ad687e0b89ed9b0ca8b13adc02228fee7
Vittal <azrial96@rambler.ru>
2015-04-13 18:05:35 +0300

    Adding round clicking

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function run(){
+    var success = document.getElementsByClassName('success');
+    for(var i=0; i < success.length; i++){
+        success[i].addEventListener('click',onSuccessClick);
+    }
+}
+function onSuccessClick(e){
+    var ref = e.currentTarget.firstChild.getAttribute('href');
+    window.location.href = ref;
+}
