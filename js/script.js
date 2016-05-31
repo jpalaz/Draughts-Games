62ef10e0a94e09a9545e4d47694afe2d606aefce
pivs96 <jpalaznik@gmail.com>
2016-05-31 21:28:15 +0300

    Add last round results

--- js/script.js
+++ js/script.js
@@ -19,8 +19,8 @@ function run() {
         $('[data-toggle="tooltip"]').tooltip()
     });
 
-    var men = ",0,1,2,0,2,2,1,0,0,,1,9\n2,,2,2,1,2,1,1,1,,2,2,16\n1,0,,0,0,0,1,1,,0,2,0,5\n0,0,2,,1,0,1,,1,1,1,0,7\n2,1,2,1,,2,,1,2,1,1,2,15\n0,0,2,2,0,,2,1,1,0,2,,10\n0,1,1,1,,0,,2,1,2,0,1,9\n1,1,1,,1,1,0,,1,0,2,1,9\n2,1,,1,0,1,1,1,,1,1,2,11\n2,,2,1,1,2,0,2,1,,2,1,14\n,0,0,1,1,0,2,0,1,0,,0,5\n1,0,2,2,0,,1,1,0,1,2,,10";
-    var women = ",1,1,2,1,0,1,0,0,0,,0,6\n1,,0,0,1,0,0,0,0,,0,0,2\n1,2,,2,1,1,0,1,,1,1,0,10\n0,2,0,,1,0,0,,1,0,0,1,5\n1,1,1,1,,1,,1,2,0,0,0,8\n2,2,1,2,1,,0,1,0,0,2,,11\n1,2,2,2,,2,,1,2,1,2,2,17\n2,2,1,,1,1,1,,0,0,2,1,11\n2,2,,1,0,2,0,2,,0,2,1,12\n2,,1,2,2,2,1,2,2,,2,2,18\n,2,1,2,2,0,0,0,0,0,,1,8\n2,2,2,1,2,,0,1,1,0,1,,12";
+    var men = ",0,1,2,0,2,2,1,0,0,1,1,10,8\n2,,2,2,1,2,1,1,1,2,2,2,18,1\n1,0,,0,0,0,1,1,1,0,2,0,6,12\n0,0,2,,1,0,1,0,1,1,1,0,7,10\n2,1,2,1,,2,2,1,2,1,1,2,17,2\n0,0,2,2,0,,2,1,1,0,2,1,11,7\n0,1,1,1,0,0,,2,1,2,0,1,9,9\n1,1,1,2,1,1,0,,1,0,2,1,11,5\n2,1,1,1,0,1,1,1,,1,1,2,12,4\n2,0,2,1,1,2,0,2,1,,2,1,14,3\n1,0,0,1,1,0,2,0,1,0,,0,6,11\n1,0,2,2,0,1,1,1,0,1,2,,11,6";
+    var women = ",1,1,2,1,0,1,0,0,0,1,0,7,10\n1,,0,0,1,0,0,0,0,0,0,0,2,12\n1,2,,2,1,1,0,1,1,1,1,0,11,7\n0,2,0,,1,0,0,1,1,0,0,1,6,11\n1,1,1,1,,1,0,1,2,0,0,0,8,9\n2,2,1,2,1,,0,1,0,0,2,1,12,6\n1,2,2,2,2,2,,1,2,1,2,2,19,2\n2,2,1,1,1,1,1,,0,0,2,1,12,5\n2,2,1,1,0,2,0,2,,0,2,1,13,3\n2,2,1,2,2,2,1,2,2,,2,2,20,1\n1,2,1,2,2,0,0,0,0,0,,1,9,8\n2,2,2,1,2,1,0,1,1,0,1,,13,4";
     var menTable = document.getElementById('men-results');
     var womenTable = document.getElementById('women-results');
 
@@ -43,7 +43,6 @@ function onSuccessClick(e) {
 
 function parseResults(results, table) {
     var data = results.data;
-    var sumPoints = [];
     for (var i = 0; i < data.length; ++i) {
         var length = data[i].length;
         for (var j = 0; j < length; ++j) {
@@ -52,18 +51,10 @@ function parseResults(results, table) {
             var className = getTdClass(i, j, data[i]);
             td.setAttribute('class', className);
             table.rows[i].appendChild(td);
-        }
-        sumPoints.push(data[i][length - 1]);
-    }
-
-    sumPoints.sort(function(a, b) {return b - a});
-    var topSums = sumPoints.slice(0, 3);
-    for (i = 0; i < data.length; ++i) {
-        var currentSum = data[i][data[i].length - 1];
-        if (topSums.indexOf(currentSum) != -1) {
-            var row = table.rows[i];
-            row.firstElementChild.setAttribute('class', 'text-danger');
-            row.lastElementChild.setAttribute('class', 'text-danger');
+            if (j + 1 == length && (data[i][j] == 1 || data[i][j] == 2 || data[i][j] == 3)) {
+                td.setAttribute('class', 'text-danger');
+                table.rows[i].firstElementChild.setAttribute('class', 'text-danger');
+            }
         }
     }
 }
@@ -71,7 +62,7 @@ function getTdClass(i, j, row) {
     var points = row[j];
     if (i === j) {
         return 'info';
-    } else if (j + 1 < row.length) {
+    } else if (j + 2 < row.length) {
         if (points == 2) {
             return 'text-danger';
         } else if (points == 1) {
